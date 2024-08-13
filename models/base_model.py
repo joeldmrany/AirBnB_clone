201881ff791e46259017dc2df22ca8c9c622d36e
joeldmrany <joeldmrany1@gmail.com>
2024-08-13 19:22:10 +0300

    debug

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,8 @@ class BaseModel():
                     self.__dict__[key] = datetime.strptime(value, time_theme)
                 else:
                     self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """ representation of the model """
