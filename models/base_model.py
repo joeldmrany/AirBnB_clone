0b1cd4eaa9e942e355140a6514dc9692c00c7af4
joeldmrany <joeldmrany1@gmail.com>
2024-08-13 18:07:09 +0300

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,13 @@ class BaseModel():
     """ Base Model calss """
 
     def __init__(self, *args, **kwargs):
-        """ for new Ba5seModel """
+        """ for new BaseModel """
         time_theme = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
-            for key, value in kwargs:
+            for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, time_theme)
                 else:
