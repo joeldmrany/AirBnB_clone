32f09da12f45b57be3235af9ea3b57b20c67d164
joeldmrany <joeldmrany1@gmail.com>
2024-08-13 18:02:28 +0300

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -15,6 +15,12 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for key, value in kwargs:
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, time_theme)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """ representation of the model """
