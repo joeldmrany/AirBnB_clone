239e55302ce08d5f6e2691311c94bc5df0fbaea4
joeldmrany <joeldmrany1@gmail.com>
2024-08-13 17:52:03 +0300

    Base Model

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+base model class
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """ Base Model calss """
+
+    def __init__(self, *args, **kwargs):
+        """ for new Ba5seModel """
+        time_theme = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def __str__(self):
+        """ representation of the model """
+        class_name = self.__class__.__name__
+        return f"{class_name} {self.id} {self.__dict__}"
+
+    def save(self):
+        """ save the model """
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        cp_dict = self.__dict__.copy()
+        cp_dict["created_at"] = self.created_at.isoformat()
+        cp_dict["updated_at"] = self.updated_at.isoformat()
+        cp_dict["__class__"] = self.__class__.__name__
+        return cp_dict
