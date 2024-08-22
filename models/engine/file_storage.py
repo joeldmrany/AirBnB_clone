394c30286910ce1325f3836db48eadc2f6867e1a
joeldmrany <joeldmrany1@gmail.com>
2024-08-22 14:39:53 +0300

    try

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -44,3 +44,5 @@ class FileStorage:
                         self.new(eval(cls_name)(**value))
         except FileNotFoundError:
             pass
+        except json.JSONDecodeError:
+            pass
